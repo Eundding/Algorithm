3260694c88c5388a5cfe39dabd292337a5a3b944
EunJeong Lee <104212013+Eundding@users.noreply.github.com>
2024-04-06 16:53:48 +0900

    [Silver IV] Title: 대피소, Time: 248 ms, Memory: 113152 KB, Score: 67 point -BaekjoonHub

---
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -0,0 +1,28 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+x = [0]*n
+y = [0]*n
+for i in range(n):
+    x[i], y[i] = map(int, input().split())
+
+INF = float("INF")
+
+def dist(c):
+    b = 0
+    for i in range(n):
+        a = 100000 #INF
+        for j in c:
+            temp = abs(x[i] - x[j]) + abs(y[i] - y[j])
+            a = min(a, temp)
+        b = max(b, a)
+    return b
+comb = list(combinations(range(n), k))
+# answer = INF
+answer = 100000
+for c in comb:
+    answer = min(answer, dist(c))
+
+print(answer)

f1f5fbecc089a729d02a5c8e8069a583f65415c7
EunJeong Lee <104212013+Eundding@users.noreply.github.com>
2024-04-26 22:23:53 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 116 ms, Memory: 109108 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    print('@'*n, end='')
+    print(' '*(3*n), end='')
+    print('@'*n)
+for i in range(n):
+    print('@'*(5*n))
+for i in range(n):
+    print('@'*n, end='')
+    print(' '*(3*n), end='')
+    print('@'*n)
+for i in range(n):
+    print('@'*(5*n))
+for i in range(n):
+    print('@'*n, end='')
+    print(' '*(3*n), end='')
+    print('@'*n)

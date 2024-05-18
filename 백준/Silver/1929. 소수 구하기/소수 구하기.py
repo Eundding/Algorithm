eace8601ce10e9e29f189d7a0e2c50ac5de2e766
EunJeong Lee <104212013+Eundding@users.noreply.github.com>
2024-05-18 15:54:59 +0900

    [Silver III] Title: 소수 구하기, Time: 212 ms, Memory: 118268 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+m, n = map(int, input().split())
+is_prime = [True]*(n+1)
+is_prime[0], is_prime[1] = False, False
+for i in range(2, n+1):
+    if is_prime[i]:
+        for j in range(i*2, n+1, i):
+            is_prime[j] = False
+
+for num in range(m, n+1):
+    if is_prime[num]:
+        print(num)

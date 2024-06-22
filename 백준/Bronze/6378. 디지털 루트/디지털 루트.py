b8d18b670b2559d216ee9f564743e941e0454f32
EunJeong Lee <104212013+Eundding@users.noreply.github.com>
2024-06-22 09:21:33 +0900

    [Bronze III] Title: 디지털 루트, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/6378. 디지털 루트/디지털 루트.py
+++ 백준/Bronze/6378. 디지털 루트/디지털 루트.py
@@ -1,17 +1,11 @@
 while True:
-    s = input()
-    if s == '0': break
-
-    while True:
-        result = 0
-        if len(s) >= 3 or (len(s) == 2 and int(s[0]) + int(s[1]) >= 10):
-            for i in range(len(s)):
-                result += int(s[i])
-            s = str(result)
-        else:
-            break
-
-    if len(s) == 1:
-        print(s[0])
-    else:
-        print(int(s[0]) + int(s[1]))
+    N = int(input())
+    if N == 0: break
+    temp = N
+    
+    while temp >= 10:
+        s = str(temp)
+        temp = 0
+        for i in range(len(s)):
+            temp += int(s[i])
+    print(temp)
